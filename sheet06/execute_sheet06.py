1272d9c5633567e9c194cf8bf44bfb8c0087beee
aikira7 <aikira7@web.de>
2017-12-07 19:36:37 +0100

    TDIDT Klassifikation eingebaut

--- sheet06/execute_sheet06.py
+++ sheet06/execute_sheet06.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.spatial.distance import cosine
 from operator import itemgetter
+from tdidt_classification import tdidt, classify_tdidt
 
 
 def read_data(path):
@@ -9,6 +10,19 @@ def read_data(path):
     labels = data_labeled[:, -1:]
 
     return data, labels
+    
+def convert_data(data_labeled):
+    # convert to format suitable for tdidt implementation
+    entries = []
+    header = list(range(1,len(data_labeled[0]-1)))
+    header.append('class_label')
+    attribute_values = {attribute: [] for attribute in header}
+    for row in data_labeled:
+        entries.append({label: float(value) for label, value in zip(header, row)})
+        for attribute, value in zip(header, row):
+            attribute_values[attribute].append(float(value))
+    header.remove('class_label')
+    return header, attribute_values, entries
 
 
 def classify(k, trainings_data, test_data, trainings_labels, test_labels):
@@ -43,6 +57,20 @@ if __name__ == '__main__':
     normalized_test_data = np.array(test_data) - training_means
     normalized_test_data /= training_std
 
-    for k in [1, 3, 5]:
-        accuracy = classify(k, normalized_trainings_data, normalized_test_data, trainings_labels, test_labels)
-        print('Accuracy for k = {}: {}'.format(k, accuracy))
+    # kNN classification
+    #for k in [1, 3, 5]:
+        #accuracy = classify(k, normalized_trainings_data, normalized_test_data, trainings_labels, test_labels)
+        #print('Accuracy for k = {}: {}'.format(k, accuracy))
+    
+    # re-attach labels to the data
+    train_data_labeled = np.concatenate((normalized_trainings_data, trainings_labels), axis=1)
+    test_data_labeled = np.concatenate((normalized_test_data, test_labels), axis=1)
+    
+    # TDIDT classification
+    depth = 5
+    header_train, attribute_values_train, entries_train = convert_data(train_data_labeled)
+    tdidt = tdidt(entries_train,  header_train, attribute_values_train, depth)
+    print(tdidt)
+    header_test, attribute_values_test, entries_test = convert_data(test_data_labeled)
+    accuracy_tdidt = classify_tdidt(tdidt, entries_test, header_test, attribute_values_test, test_labels)
+    print('Accuracy des Decision Tree mit Tiefe ' + str(depth) + ': ' + str(accuracy_tdidt))
---
+++ sheet06/tdidt_classification.py
@@ -0,0 +1,115 @@
+from math import log
+from operator import itemgetter
+
+# code copied from sheet02, only slightly modified
+
+def _is_perfectly_classified(samples):
+    value = samples[0]['class_label']
+    for s in samples:
+        if s['class_label'] != value:
+            return False
+    return True
+
+
+def _pos_classification_count(samples):
+    positive = 0
+    for sample in samples:
+        if sample['class_label'] == 1:
+            positive += 1
+    return positive
+
+
+def _pos_classification_fraction(samples):
+    positive = _pos_classification_count(samples)
+    # avoid division by 0
+    if len(samples) == 0:
+        res = 0
+    else:
+        res = positive / len(samples)
+    return res
+
+
+def _entropy(samples, sample_subset, pos_fraction):
+    # avoid division by 0
+    if pos_fraction == 0 or pos_fraction == 1:
+        return 0
+    return (len(sample_subset) / len(samples)) * (
+    (pos_fraction * log(1 / pos_fraction, 2)) + ((1 - pos_fraction) * log(1 / (1 - pos_fraction), 2)))
+
+
+def _split_samples(samples, attribute, threshold):
+    samples_below = [x for x in samples if x[attribute] <= threshold]
+    samples_above = [x for x in samples if x[attribute] > threshold]
+    return samples_below, samples_above
+
+
+def _inverted_gain(samples, attribute, threshold):
+    samples_below, samples_above = _split_samples(samples, attribute, threshold)
+    _pos_below_frac = _pos_classification_fraction(samples_below)
+    _pos_above_frac = _pos_classification_fraction(samples_above)
+
+    return _entropy(samples, samples_below, _pos_below_frac) + _entropy(samples, samples_above, _pos_above_frac)
+
+
+def _majority_class(pos_samples, neg_samples):
+    if pos_samples >= neg_samples:
+        return 1.0
+    else:
+        return 0.0
+
+def tdidt(samples, attributes, attribute_values, max_depth):
+    count_positive = _pos_classification_count(samples)
+    count_negative = len(samples) - count_positive
+    if _is_perfectly_classified(samples):
+        return {'value': samples[0]['class_label'], 'left': None, 'right': None, 'samples': len(samples),
+                'pos_samples': count_positive, 'neg_samples': count_negative, 'attribute': None, 'threshold': None,
+                'gain': None, 'samples_test': None, 'pos_samples_test': None, 'neg_samples_test': None}
+
+    if max_depth == 0:
+        value = _majority_class(count_positive, count_negative)
+        return {'value': value, 'left': None, 'right': None, 'samples': len(samples), 'pos_samples': count_positive,
+                'neg_samples': count_negative, 'attribute': None, 'threshold': None, 'gain': None, 'samples_test': None,
+                'pos_samples_test': None, 'neg_samples_test': None}
+
+    candidates = []
+    for attribute in attributes:
+        for threshold in attribute_values[attribute]:
+            candidates.append((attribute, threshold, _inverted_gain(samples, attribute, threshold)))
+
+    best_attribute, best_threshold, best_gain = min(candidates, key=itemgetter(2))
+    samples_below, samples_above = _split_samples(samples, best_attribute, best_threshold)
+
+    return {
+        'value': None,  # Added for converting Node to leaf
+        'attribute': best_attribute,
+        'threshold': best_threshold,
+        'gain': best_gain,
+        'samples': len(samples), 'pos_samples': count_positive, 'neg_samples': count_negative,
+        'samples_test': None, 'pos_samples_test': None, 'neg_samples_test': None,
+        'left': tdidt(samples_below, attributes, attribute_values, max_depth - 1),
+        'right': tdidt(samples_above, attributes, attribute_values, max_depth - 1)
+    }
+
+def _is_leaf(tree):
+    return tree['left'] is None and tree['right'] is None
+
+
+def classify_one_sample(tree, sample, sample_index, attributes, attribute_values):
+    if _is_leaf(tree):
+        return tree['value']
+    else:
+        if attribute_values[tree['attribute']][sample_index] <= tree['threshold']:
+            return classify_one_sample(tree['left'], sample, sample_index, attributes, attribute_values)
+        else:
+            return classify_one_sample(tree['right'], sample, sample_index, attributes, attribute_values)
+
+
+def classify_tdidt(tree, samples, attributes, attribute_values, class_labels):
+    res = []
+    for i in range(len(samples)):
+        res.append(classify_one_sample(tree, samples[i], i, attributes, attribute_values))
+    true_classified = 0
+    for i in range(len(res)):
+        if res[i] == class_labels[i]:
+            true_classified += 1
+    return true_classified / len(res)
