07cfa2fd14e4373ea0db4b084ad47b5c32764199
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-12-07 17:13:41 +0100

    Teil 3.1 komplett

--- sheet06/execute_sheet06.py
+++ sheet06/execute_sheet06.py
@@ -1,34 +1,48 @@
 import numpy as np
 from scipy.spatial.distance import cosine
 from operator import itemgetter
-from itertools import product
-from pprint import pprint
 
-if __name__ == '__main__':
-    k = 3
-    trainings_data_labeled = np.genfromtxt('test.csv', delimiter=',')
-    trainings_data = trainings_data_labeled[:, :-1]
-    trainings_labels = trainings_data_labeled[:, -1:]
-    print(trainings_labels)
-    training_means = np.mean(trainings_data, 0)
-    training_std = np.std(trainings_data, 0)
-    normalized_trainings_data = np.array(trainings_data) - training_means
 
-    normalized_trainings_data /= training_std
+def read_data(path):
+    data_labeled = np.genfromtxt(path, delimiter=',')
+    data = data_labeled[:, :-1]
+    labels = data_labeled[:, -1:]
 
-    print(normalized_trainings_data)
-    print()
-    print(normalized_trainings_data[:, :-1])
-    classifications = []
+    return data, labels
 
-    for sample_index, sample in enumerate(normalized_trainings_data):
-        d = [(i, cosine(normalized_trainings_data[i, :], sample)) for i in range(len(normalized_trainings_data))]
+
+def classify(k, trainings_data, test_data, trainings_labels, test_labels):
+    correct_classifications = 0
+    for sample_index, sample in enumerate(test_data):
+        d = [(i, cosine(trainings_data[i, :], sample)) for i in range(len(trainings_data))]
         d.sort(key=itemgetter(1))
-        neighbours = d[:k+1]
+        neighbours = d[:k + 1]
 
         positive = 0
-        print(neighbours)
         for i, value in neighbours:
-            if trainings_labels[i] == 1 and i != sample_index:
+            if trainings_labels[i] == 1:
                 positive += 1
-        print(positive)
+
+        classification = 1 if positive > k - positive else 0
+        if classification == test_labels[sample_index]:
+            correct_classifications += 1
+
+    accuracy = correct_classifications / len(test_data)
+    return accuracy
+
+
+if __name__ == '__main__':
+    trainings_data, trainings_labels = read_data('spam_training.csv')
+    test_data, test_labels = read_data('spam_test.csv')
+
+    training_means = np.mean(trainings_data, 0)
+    training_std = np.std(trainings_data, 0)
+
+    normalized_trainings_data = np.array(trainings_data) - training_means
+    normalized_trainings_data /= training_std
+    normalized_test_data = np.array(test_data) - training_means
+    normalized_test_data /= training_std
+
+    for k in [1, 3, 5]:
+        accuracy = classify(k, normalized_trainings_data, normalized_test_data, trainings_labels, test_labels)
+        print('Accuracy for k = {}: {}'.format(k, accuracy))
